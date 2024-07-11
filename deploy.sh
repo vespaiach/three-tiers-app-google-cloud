ee87ce5883ac646ec8becebe3656c271d71915c4
Trinh Nguyen <trinh@junecare.co>
2024-07-11 21:13:40 +0000

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 IMAGE_TAG="1.0.0"
 cd ./frontend
 gcloud builds submit --config=./cloudbuild.yaml --substitutions=_IMAGE_TAG="${IMAGE_TAG}"
+cd -
+cd ./backend
+gcloud builds submit --config=./cloudbuild.yaml --substitutions=_IMAGE_TAG="${IMAGE_TAG}"
