9aead65472141f15413a19b11ebe72e3ebbeb505
Trinh Nguyen <trinh@junecare.co>
2024-07-11 17:44:05 -0400

    use secret manager

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
-IMAGE_TAG="1.0.0"
+IMAGE_TAG="${1:-1.0.0}"
+
+# Fetch the RAILS_MASTER_KEY from Google Secret Manager
+RAILS_MASTER_KEY=$(gcloud secrets versions access latest --secret=RAILS_MASTER_KEY)
+
 cd ./frontend
 gcloud builds submit --config=./cloudbuild.yaml --substitutions=_IMAGE_TAG="${IMAGE_TAG}"
 cd -
 cd ./backend
-gcloud builds submit --config=./cloudbuild.yaml --substitutions=_IMAGE_TAG="${IMAGE_TAG}"
+gcloud builds submit --config=./cloudbuild.yaml --substitutions=_IMAGE_TAG="${IMAGE_TAG} --substitutions=_RAILS_MASTER_KEY="${RAILS_MASTER_KEY}"
