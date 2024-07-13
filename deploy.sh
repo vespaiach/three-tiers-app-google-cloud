48de96e0e20bed9b3147e9f23f442f6ad88d3695
Trinh Nguyen <trinh@junecare.co>
2024-07-13 05:47:10 -0400

    default region

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 IMAGE_TAG="${1:-1.0.0}"
 PROJECT_ID="ahhahihi"
 PROJECT_NUMBER="660837874699"
+REGION="us-west1"
 
 # gcloud config set project $PROJECT_ID
 # gcloud services enable compute.googleapis.com
@@ -19,7 +20,7 @@ PROJECT_NUMBER="660837874699"
 # echo -n "14230907274cb48c1e28d6086424d9d0" | gcloud secrets versions add RAILS_MASTER_KEY --data-file=-
 
 # set default region
-gcloud config set run/region us-west1
+gcloud config set run/region $REGION
 
 RAILS_MASTER_KEY=$(gcloud secrets versions access latest --secret=RAILS_MASTER_KEY)
 TODO_DB_USER=$(gcloud secrets versions access latest --secret="TODO_DB_USER")
@@ -38,20 +39,20 @@ gcloud builds submit \
     --config=./cloudbuild.yaml \
     --substitutions=_IMAGE_TAG="${IMAGE_TAG}",_RAILS_MASTER_KEY="${RAILS_MASTER_KEY}",_TODO_DB_USER="${TODO_DB_USER}",_TODO_DB_USER_PASSWORD="${TODO_DB_USER_PASSWORD},_TODO_DB_HOST=${TODO_DB_HOST}"
 
-cd -
-gcloud run deploy three-tier-fe \
-    --image=gcr.io/${PROJECT_ID}/three-tier-app-fe:${IMAGE_TAG} \
-    --allow-unauthenticated \
-    --port=80 \
-    --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
-    --region=us-central1 \
-    --project=$PROJECT_ID
+# cd -
+# gcloud run deploy three-tier-fe \
+#     --image=gcr.io/${PROJECT_ID}/three-tier-app-fe:${IMAGE_TAG} \
+#     --allow-unauthenticated \
+#     --port=80 \
+#     --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
+#     --region=${REGION} \
+#     --project=$PROJECT_ID
 
-cd -
-gcloud run deploy three-tier-be \
-    --image=gcr.io/${PROJECT_ID}/three-tier-app-be:${IMAGE_TAG} \
-    --allow-unauthenticated \
-    --port=80 \
-    --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
-    --region=us-central1 \
-    --project=$PROJECT_ID
+# cd -
+# gcloud run deploy three-tier-be \
+#     --image=gcr.io/${PROJECT_ID}/three-tier-app-be:${IMAGE_TAG} \
+#     --allow-unauthenticated \
+#     --port=80 \
+#     --service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com \
+#     --region=${REGION} \
+#     --project=$PROJECT_ID
