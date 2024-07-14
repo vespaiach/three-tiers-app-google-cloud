bb127e1827ea72859225cccd450ec346b82020b1
Trinh Nguyen <trinh@junecare.co>
2024-07-13 20:53:19 -0400

    fix cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080"
+    origins "http://localhost:8080",
             "http://127.0.0.1:8080",
             "https://www.letbike.com",
             "https://letbike.com"
