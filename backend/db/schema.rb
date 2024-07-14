5cf99811a597cbe8255c04a49125ef0d8b0347f1
Trinh Nguyen <trinh@junecare.co>
2024-07-14 16:34:28 -0400

    add seed script

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_151844) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_14_182832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rewards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todo_items", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "due"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vitals", force: :cascade do |t|
+    t.integer "weight"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
